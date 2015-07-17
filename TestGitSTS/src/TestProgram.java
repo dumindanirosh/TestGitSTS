3b8928d5776292ef9ddce08a9d443a2c3cc67c1d
dumindanirosh <duminda.nirosh@gmail.com>
2015-07-17 20:49:44 +0530

    Second Change

--- TestGitSTS/src/TestProgram.java
+++ TestGitSTS/src/TestProgram.java
@@ -4,6 +4,7 @@ public class TestProgram {
 	public static void main(String[] args) {
 	
 		System.out.println("Hi Git Hub"); // Edited by GIT Login
+		System.out.println("Hi Git Hubbbbbbbbb");
 
 	}
 
